1a3bfdcd4b00588ca60c8cf244833930b06b9111
Shumskih <music.dll@mail.ru>
2017-10-21 22:47:03 +0300

    minor changes

--- src/5.sql
+++ src/5.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS first_table, second_table;
 
 CREATE TEMP TABLE first_table AS (
-  SELECT company_id AS company_id, customer_id AS customer_id, customers.name AS customer_name, projects.id, sum(projects.cost) AS sum
+  SELECT company_id AS company_id, customer_id AS customer_id, customers.name AS customer_name, projects.id, projects.cost AS project_cost
   FROM projects
     INNER JOIN companies_projects ON projects.id = companies_projects.project_id
     INNER JOIN customers_projects ON companies_projects.project_id = customers_projects.project_id
@@ -11,13 +11,13 @@ CREATE TEMP TABLE first_table AS (
 
 
 CREATE TEMP TABLE second_table AS (
-  SELECT company_id, min(sum) AS min_sum
+  SELECT company_id, min(project_cost) AS min_sum
   FROM first_table
   GROUP BY company_id
 );
 
 
-SELECT first_table.customer_id, first_table.company_id, first_table.customer_name, first_table.sum
+SELECT first_table.company_id, first_table.customer_id, first_table.customer_name, first_table.project_cost
 FROM first_table
-  INNER JOIN second_table ON first_table.sum = second_table.min_sum
-                           AND first_table.company_id = second_table.company_id
+  INNER JOIN second_table ON first_table.project_cost = second_table.min_sum
+                             AND first_table.company_id = second_table.company_id
