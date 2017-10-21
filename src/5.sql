e031f68f36e671d24d918fb225a57fec31eb6862
Shumskih <music.dll@mail.ru>
2017-10-21 11:40:49 +0300

    Solution of 5.sql

--- src/5.sql
+++ src/5.sql
@@ -1,26 +1,23 @@
-SELECT companies.name AS company, projects.name AS project, min(projects.cost), customers.name AS customer
-FROM companies
-  INNER JOIN companies_projects_customers ON companies.id = companies_projects_customers.company_id
-  INNER JOIN projects ON projects.id = companies_projects_customers.project_id
-  INNER JOIN customers ON customers.id = companies_projects_customers.customer_id
-WHERE companies.name = 'Ars24'
-GROUP BY companies.name, projects.name, customers.name, projects.cost
-LIMIT 1;
+DROP TABLE IF EXISTS profit, min_profit;
 
-SELECT companies.name AS company, projects.name AS project, min(projects.cost), customers.name AS customer
-FROM companies
-  INNER JOIN companies_projects_customers ON companies.id = companies_projects_customers.company_id
-  INNER JOIN projects ON projects.id = companies_projects_customers.project_id
-  INNER JOIN customers ON customers.id = companies_projects_customers.customer_id
-WHERE companies.name = 'Dkrs'
-GROUP BY companies.name, projects.name, customers.name, projects.cost
-LIMIT 1;
+CREATE TEMP TABLE profit AS (
+  SELECT company_id AS company_id, customer_id AS customer_id, customers.name AS customer_name, projects.id, sum(projects.cost) AS sum
+  FROM projects
+    INNER JOIN companies_projects ON projects.id = companies_projects.project_id
+    INNER JOIN customers_projects ON companies_projects.project_id = customers_projects.project_id
+    INNER JOIN customers ON customers.id = customers_projects.customer_id
+  GROUP BY company_id, customer_id, projects.id, customer_name
+);
 
-SELECT companies.name AS company, projects.name AS project, min(projects.cost), customers.name AS customer
-FROM companies
-  INNER JOIN companies_projects_customers ON companies.id = companies_projects_customers.company_id
-  INNER JOIN projects ON projects.id = companies_projects_customers.project_id
-  INNER JOIN customers ON customers.id = companies_projects_customers.customer_id
-WHERE companies.name = 'Go Staff'
-GROUP BY companies.name, projects.name, customers.name, projects.cost
-LIMIT 1;
+
+CREATE TEMP TABLE min_profit AS (
+  SELECT company_id, min(sum) AS min_cost
+  FROM profit
+  GROUP BY company_id
+);
+
+
+SELECT profit.customer_id, profit.company_id, profit.customer_name, profit.sum
+FROM profit
+  INNER JOIN min_profit ON profit.sum = min_profit.min_cost
+                           AND profit.company_id = min_profit.company_id
